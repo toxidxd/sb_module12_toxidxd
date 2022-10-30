b61596ff43c3aea8d840d4c5c26cc4a25e4a5226
toxidxd <toxid13@yandex.ru>
2022-10-30 20:34:41 +0300

    done

--- task_8.py
+++ task_8.py
@@ -1,3 +1,32 @@
 print('Задача 8. НОД')
+# Напишите функцию, вычисляющую наибольший общий делитель двух чисел
 
-#Напишите функцию, вычисляющую наибольший общий делитель двух чисел
+
+def gcd_finder(small_num, large_num):
+    print('gcd_finder')
+    if large_num % small_num != 0:
+        remains = large_num % small_num
+        new_large_num = small_num
+        remains = gcd_finder(remains, new_large_num)
+    else:
+        remains = small_num
+
+    return remains
+
+
+f_num = int(input("Введите первое число: "))
+s_num = int(input("Введите второе число: "))
+gcd = 0
+
+if f_num <= 0 or s_num <= 0:
+    print("Числа должны быть положительными!")
+else:
+    max_num = int((f_num + s_num) / 2 + abs(f_num - s_num) / 2)
+    min_num = int((f_num + s_num) / 2 - abs(f_num - s_num) / 2)
+
+    if max_num % min_num == 0:
+        gcd = min_num
+    else:
+        gcd = gcd_finder(min_num, max_num)
+
+    print(f"НОД равен {gcd}")
